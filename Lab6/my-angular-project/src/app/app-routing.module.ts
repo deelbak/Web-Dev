2da3219ffd580409b915680eb2212a4d7ff88907
Adilbek <zhumazhanov40@gmail.com>
2023-03-08 17:28:23 +0600

    62th commit

---
+++ Lab6/my-angular-project/src/app/app-routing.module.ts
@@ -0,0 +1,9 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+
+import { HomeComponent } from './home/home.component';
+import { AboutComponent } from './about/about.component';
+import { AlbumsComponent } from './albums/albums.component';
+import { AlbumDetailComponent } from './album-detail/album-detail.component';
+import { AlbumPhotosComponent } from './album-photos/album-photos.component';
+
