22243f60b6eeff50c2ea5693d8bc7132a43a44dd
Adilbek <zhumazhanov40@gmail.com>
2023-03-08 17:29:20 +0600

    62th commit

--- Lab6/my-angular-project/src/app/app-routing.module.ts
+++ Lab6/my-angular-project/src/app/app-routing.module.ts
@@ -7,3 +7,17 @@ import { AlbumsComponent } from './albums/albums.component';
 import { AlbumDetailComponent } from './album-detail/album-detail.component';
 import { AlbumPhotosComponent } from './album-photos/album-photos.component';
 
+const routes: Routes = [
+  { path: '', redirectTo: '/home', pathMatch: 'full' },
+  { path: 'home', component: HomeComponent },
+  { path: 'about', component: AboutComponent },
+  { path: 'albums', component: AlbumsComponent },
+  { path: 'albums/:id', component: AlbumDetailComponent },
+  { path: 'albums/:id/photos', component: AlbumPhotosComponent },
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
